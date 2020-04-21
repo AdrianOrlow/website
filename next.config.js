001a4ebed007b996b77e8d99e2065303e1c9fa27
Adrian Orłów <adrian@orlow.me>
2020-04-21 21:19:05 +0200

    Update Next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
-require('dotenv').config();
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 
 module.exports = {
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
 
-    return config;
-  },
-  env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    RECAPTCHA_SERVER_KEY: process.env.RECAPTCHA_SERVER_KEY,
-    RECAPTCHA_CLIENT_KEY: process.env.RECAPTCHA_CLIENT_KEY,
-    GA_TAG: process.env.GA_TAG,
-  },
+    return config
+  }
 };
