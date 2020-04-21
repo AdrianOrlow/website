0e8a4bbd5e58c150275283785e55a9e3d7b4c716
Adrian Orłów <adrian@orlow.me>
2020-04-21 17:44:44 +0200

    Add Google Analytics

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-require('dotenv').config();
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
 
-module.exports = {
+dotenvLoad();
+
+const withNextEnv = nextEnv();
+
+module.exports = withNextEnv({
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -13,5 +18,6 @@ module.exports = {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     RECAPTCHA_CLIENT_KEY: process.env.RECAPTCHA_CLIENT_KEY,
     RECAPTCHA_SERVER_KEY: process.env.RECAPTCHA_SERVER_KEY,
+    GA_TAG: process.env.GA_TAG,
   }
-};
+});
